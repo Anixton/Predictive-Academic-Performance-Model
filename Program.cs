dd08a30acc0a3c8531de2f02f4b14180252cb1f8
Anixton <hakangezginci35@gmail.com>
2023-11-13 19:22:32 +0300

    Added Prediction Feature

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,19 +41,28 @@ namespace Puan_Tahmini
             {8.1, 11, 82}
         };
 
-        public Neuron(double weight1, double weight2)
+        public Neuron()
         {
-            weightOfStudy = weight1;
-            weightOfAttendance = weight2;
+
+            // random positive values between 0.0 and 1
+            Random random = new Random((int)DateTime.Now.Ticks);
+            double randomDouble1 = random.NextDouble();
+            double randomDouble2 = random.NextDouble();
+
+           
+
+
+            weightOfStudy = randomDouble1;
+            weightOfAttendance = randomDouble2;
 
             bias = 0;
             mse = 0;
 
             for (int i = 0; i < 21; i++)
             {
-                doubleArray[i, 0] /= 10;    // Divide the first column by 10
-                doubleArray[i, 1] /= 15;    // Divide the second column by 15
-                doubleArray[i, 2] /= 100;   // Divide the last column by 100
+                doubleArray[i, 0] /= 10;    
+                doubleArray[i, 1] /= 15;    
+                doubleArray[i, 2] /= 100;   
             }
 
         }
@@ -68,6 +79,7 @@ namespace Puan_Tahmini
             for (int epoch = 0; epoch < epochs; epoch++)
             {
                 double totalError = 0;
+                mse = 0;
 
                 for (int i = 0; i < doubleArray.GetLength(0); i++)
                 {
@@ -87,18 +99,55 @@ namespace Puan_Tahmini
                     bias += deltaBias;
 
                     totalError += error;
-                    mse+= error * error;
+                    mse+= error * error;                
                 }
 
                 mse /= doubleArray.GetLength(0);
 
-                Console.WriteLine($"Epoch {epoch + 1}, Total Error: {totalError}, MSE: {this.getMSE()}");
+                Console.WriteLine($"Epoch {epoch + 1}, Total Error: {totalError}, MSE: {this.getMSE()}");             
             }
+            targetExpectedComparison();
+        }
+
+        private void targetExpectedComparison()
+        {
+            // Display column labels with fixed-width columns
+            Console.WriteLine("Çalışma        Derse Katılma     Sınav Sonucu      Tahmin");
+
+            // Format and print the model's output for the same data with fixed-width columns
+            for (int i = 0; i < this.doubleArray.GetLength(0); i++)
+            {
+                double input1 = this.doubleArray[i, 0];
+                double input2 = this.doubleArray[i, 1];
+                double target = this.doubleArray[i, 2];
+                double predictedOutput = this.ComputeOutput(input1, input2);
+
+                // Use fixed-width columns for each value             
+                Console.WriteLine($"{input1 * 10,-15:F2}  {input2 * 15,-15:F2}  {target * 100,-15:F2}  {predictedOutput * 100,-15:F2}");
+
+
+            }
+
+            // returns Mean Square Error
+            Console.WriteLine($"MSE: {this.getMSE()}");
         }
 
         public double getMSE()
         {
             return mse;
+        }
+
+        public void makePrediction(double studyTime, double attendanceTime)
+        {
+            double firstInput = (studyTime / 10);
+            double secondInput= (attendanceTime / 15);
+
+            double expectedVal=ComputeOutput(firstInput, secondInput)*100;
+
+            Console.WriteLine($"Study Time: {studyTime}");
+            Console.WriteLine($"Attendance: {attendanceTime}");
+            Console.WriteLine($"Prediction: {expectedVal}");
+            Console.WriteLine();
         }
     }
 
@@ -108,40 +157,25 @@ namespace Puan_Tahmini
 
         static void Main(string[] args)
         {
-            double[] inputs = { 2.0, 3.0 };
+            Neuron ANN = new Neuron();
 
-            // random positive values between 0.0 and 1
-            int seed = 42;
-            Random random = new Random(seed);
-            double randomDouble1 = random.NextDouble(); // Generates a random double between 0 (inclusive) and 1 (exclusive)
-            double randomDouble2 = random.NextDouble(); // Generates another random double
 
-            double[] weights = { randomDouble1 , randomDouble2};
-
-            Neuron ANN = new Neuron(randomDouble1, randomDouble1);
-
-            Console.WriteLine(ANN.ComputeOutput(inputs[0], inputs[1]));
 
             ANN.Train(0.005, 10);
 
+            ANN.makePrediction(9, 8);
+            ANN.makePrediction(7.5, 10);
+            ANN.makePrediction(9, 9);
+            ANN.makePrediction(8, 11);
+            ANN.makePrediction(10, 10);
 
-            // Display column labels with fixed-width columns
-            Console.WriteLine("First Data         Second Data        Target Value       Predicted Output");
 
-            // Format and print the model's output for the same data with fixed-width columns
-            for (int i = 0; i < ANN.doubleArray.GetLength(0); i++)
-            {
-                double input1 = ANN.doubleArray[i, 0];
-                double input2 = ANN.doubleArray[i, 1];
-                double target = ANN.doubleArray[i, 2];
-                double predictedOutput = ANN.ComputeOutput(input1, input2);
 
-                // Use fixed-width columns for each value
-                Console.WriteLine($"{input1,-20:F2}  {input2,-20:F2}  {target,-20:F2}  {predictedOutput,-20:F2}");
-            }
 
-            // returns Mean Square Error
-            Console.WriteLine(ANN.getMSE());
+
+
+
+
 
             // write MSE formula 
             // In Statistics, Mean Squared Error (MSE) is defined as Mean or
@@ -153,6 +187,7 @@ namespace Puan_Tahmini
 
             // resource
             // https://www.mygreatlearning.com/blog/mean-square-error-explained/#:~:text=In%20Statistics%2C%20Mean%20Squared%20Error,between%20actual%20and%20estimated%20values.
+
 
         }
     }

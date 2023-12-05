6c3da57f1cdd51378933cd3382d62ecb4b545ac6
Hakan Gezginci <121567782+Anixton@users.noreply.github.com>
2023-12-05 19:02:56 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Puan_Tahmini")]
+[assembly: AssemblyTitle("Predictive_Academic_Performance_Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Puan_Tahmini")]
+[assembly: AssemblyProduct("Predictive_Academic_Performance_Model")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
